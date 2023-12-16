b185ad11e1aa6650a251f0dad7bea4a966ea3b94
chintan patel <imchintan0207@gmail.com>
2023-12-16 15:19:10 +0530

    BookStore Intial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { UpdateBookComponent } from './update-book/update-book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { MybookComponent } from './mybook/mybook.component';
+import { authGuard } from './auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'booklist', component: BookListComponent},
+  {path:'addbook', component: AddBookComponent, canActivate: [authGuard]},
+  {path:'updatebook/:id', component: UpdateBookComponent},
+  {path: 'bookdetails/:id', component: BookDetailsComponent},
+  {path: 'mybooks', component:MybookComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UpdateBookComponent } from './update-book/update-book.component';
+import { MybookComponent } from './mybook/mybook.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BookListComponent,
+    AddBookComponent,
+    BookDetailsComponent,
+    UpdateBookComponent,
+    MybookComponent,
+    LoginComponent,
+    RegisterComponent
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,HttpClientModule,FormsModule,ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
